c7a893e3b212dddead79a11186d7f90b09e010ea
haniyan <hania-nocun@o2.pl>
2022-02-03 21:17:52 +0100

    finding hitboxes position

--- public/js/app.js
+++ public/js/app.js
@@ -2,60 +2,73 @@
 
 const viewfinder = document.querySelector(".viewfinder");
 const troll = document.querySelector(".troll");
-const startBtn = document.querySelector(".start")
-const stopBtn = document.querySelector(".stop")
-const shotBtn = document.querySelector(".shot")
+const startBtn = document.querySelector(".start");
+const stopBtn = document.querySelector(".stop");
+const shotBtn = document.querySelector(".shot");
+const scoreNumberBox = document.querySelector(".score-number");
+const headHitBox = document.querySelector(".head");
+const bellyHitBox = document.querySelector(".belly");
+const legHitBox = document.querySelector(".leg");
+const leftArmHitBox = document.querySelector(".arm-left");
+const rightArmHitBox = document.querySelector(".arm-right");
 
-// const divTenPosition = {
-//     left: ,
-//     right: ,
-//     top: ,
-//     bottom: ,
-// }
-// const divNinePosition = {
-//     left: ,
-//     right: ,
-//     top: ,
-//     bottom: ,
-// }
-// const divEightPosition = {
-//     left: ,
-//     right: ,
-//     top: ,
-//     bottom: ,
-// }
-// const divSevenPosition = {
-//     left: ,
-//     right: ,
-//     top: ,
-//     bottom: ,
-// }
-// const divSixPosition = {
-//     left: ,
-//     right: ,
-//     top: ,
-//     bottom: ,
-// }
-const divFifePosition = {
-    left: 473,
-    right: 837,
-    top: 452,
-    bottom: 809,
+console.log(headHitBox.offsetTop)
+
+const viewfinderPosition = {
+    left:,
+    right:,
+    width: 140,
+    height: 160,
 }
 
-function shotPosition (){
+const headHitBoxPosition = {
+    left: headHitBox.offsetLeft,
+    top: headHitBox.offsetTop,
+    width: 175,
+    height: 180,
+}
+const bellyHitBoxPosition = {
+    left: bellyHitBox.offsetLeft,
+    top: bellyHitBox.offsetTop,
+    width: 175,
+    height: 200,
+}
+const legHitBoxPosition = {
+    left: legHitBox.offsetLeft,
+    top: legHitBox.offsetTop,
+    width: 175,
+    height: 100,
+}
+const leftArmHitBoxPosition = {
+    left: leftArmHitBox.offsetLeft,
+    top: leftArmHitBox.offsetTop,
+    width: 200,
+    height: 300,
+}
+const rightArmHitBoxPosition = {
+    left: rightArmHitBox.offsetLeft,
+    top: rightArmHitBox.offsetTop,
+    width: 150,
+    height: 250,
+}
+
+let score = 0;
+
+function shotPosition() {
 
 
     let currentPositionX = viewfinder.offsetLeft;
     let currentPositionY = viewfinder.offsetTop;
 
-    console.log(currentPositionX,currentPositionY)
+    console.log(currentPositionX, currentPositionY)
 
 
-    if(currentPositionX>=0 && currentPositionX<=1000 && currentPositionY >=0&& currentPositionY <= 1000){
+    if (currentPositionX >= 0 && currentPositionX <= 1000 && currentPositionY >= 0 && currentPositionY <= 1000) {
         console.log("+10 pkt");
-    }
-    else {
+        score = score + 10;
+        scoreNumberBox.innerHTML = `${score}`;
+
+    } else {
         console.log("looser");
     }
     troll.style.animation = "trollDie 2s infinite linear alternate";
@@ -63,18 +76,14 @@ function shotPosition (){
 
 }
 
-function trollMoves(){
 
-
-}
-
-startBtn.addEventListener("click", ()=>{
+startBtn.addEventListener("click", () => {
     viewfinder.style.animation = "move 8s infinite linear alternate, moveTwo 8s infinite alternate"
     troll.style.animation = "trollBasic 3s infinite linear alternate"
 })
 
-stopBtn.addEventListener("click", ()=>{
-    viewfinder.style.animation =  "paused";
+stopBtn.addEventListener("click", () => {
+    viewfinder.style.animation = "paused";
 
 })
 
