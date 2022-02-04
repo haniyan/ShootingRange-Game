cce1e273256a81bed75a24559aaca9b1c0cdceb8
haniyan <hania-nocun@o2.pl>
2022-02-04 14:13:38 +0100

    progress bar working

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,26 @@
 
 const viewfinder = document.querySelector(".viewfinder");
 const troll = document.querySelector(".troll");
+
 const startBtn = document.querySelector(".start");
 const stopBtn = document.querySelector(".stop");
 const shotBtn = document.querySelector(".shot");
+
 const scoreNumberBox = document.querySelector(".score-number");
+
 const headHitBox = document.querySelector(".head");
 const bellyHitBox = document.querySelector(".belly");
 const legHitBox = document.querySelector(".leg");
 const leftArmHitBox = document.querySelector(".arm-left");
 const rightArmHitBox = document.querySelector(".arm-right");
 
-console.log(headHitBox.offsetTop)
 
-const viewfinderPosition = {
-    width: 140,
-    height: 160,
-}
+const progressDone = document.querySelector(".progress-done");
+const progressBar = document.querySelector(".progress");
+
+let score = 0;
+const lifeScore = 600;
+
 
 const headHitBoxPosition = {
     left: headHitBox.offsetLeft,
@@ -50,18 +54,14 @@ const rightArmHitBoxPosition = {
     height: 250,
 }
 
-let score = 0;
 
-console.log(headHitBoxPosition.top, bellyHitBoxPosition.top)
+
 function shotPosition() {
 
     //position in the moment of function call
     let currentX = viewfinder.offsetLeft;
     let currentY = viewfinder.offsetTop;
-    let currentHeight = viewfinder.offsetTop - viewfinderPosition.height;
-    let currentWidth = viewfinder.offsetLeft + viewfinderPosition.width;
 
-    console.log(currentX, currentY)
     //head - collision detect
     if (currentX < headHitBoxPosition.left + headHitBoxPosition.width && currentX > headHitBoxPosition.left
         && currentY < headHitBoxPosition.top + headHitBoxPosition.height && currentY > headHitBoxPosition.top) {
@@ -112,22 +112,34 @@ function shotPosition() {
         console.log("looser");
     }
 
-
+    TrollIsDead()
+    checkLifeScore()
 }
 
 function TrollIsDead() {
-    troll.style.animation = "trollDie 2s infinite linear alternate";
+    if (score>=lifeScore){
+        troll.style.animation = "trollDie 2s 1 linear alternate";
+
+        setTimeout(()=>{
+            viewfinder.style.animation = "paused";
+            troll.style.animation ="paused"
+        }, 3000)
+
+
+    }
+// + trollBasic paused + div winner
+
 }
 
 
 startBtn.addEventListener("click", () => {
-    viewfinder.style.animation = "move 10s infinite linear alternate, moveTwo 10s infinite alternate"
-    // troll.style.animation = "trollBasic 8s infinite linear alternate"
+    // viewfinder.style.animation = "move 12s infinite linear alternate, moveTwo 20s infinite alternate"
+    troll.style.animation = "trollBasic 2s infinite linear alternate"
+
 })
 
 stopBtn.addEventListener("click", () => {
     viewfinder.style.animation = "paused";
-
 })
 
 shotBtn.addEventListener("click", shotPosition)
@@ -135,3 +147,27 @@ shotBtn.addEventListener("click", shotPosition)
 
 //trzeba znaleźć dobrą pozycję gdzie to wrzucić
 // viewfinder.style.animation = "weaponRotate 0.2s 1 linear alternate";
+
+
+
+
+
+function checkLifeScore(){
+
+if(score<=lifeScore){
+
+    progressDone.style.width = score + 'px';
+    progressDone.innerText = `${score}/${lifeScore}`;
+
+    if(score>=100){
+
+        progressDone.style.fontSize = "1.5em";
+        progressDone.style.opacity = 1;
+    }
+
+}
+else{
+    progressDone.innerText = "WYGRANA!";
+}
+
+}
